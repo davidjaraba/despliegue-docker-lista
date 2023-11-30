c0b30b52acd317363718f71ccfd7b467ce0d60b7
David Jaraba <velcas669@gmail.com>
2023-11-30 17:43:47 +0100

    terminadas funcionalidades

---
+++ database/listas.js
@@ -0,0 +1,14 @@
+db.createUser({
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    roles: [
+        {
+            role: 'readWrite',
+            db: process.env.MONGO_INITDB_DATABASE,
+        },
+    ],
+});
+
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+
+db.createCollection('tareas');
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
+const port = process.env.PORT || 4000;
 
+const dbUser = process.env.DATABASE_USER || 'postgres';
+const dbPassword = process.env.DATABASE_PASSWORD || 'supersecurepassword';
+const dbName = process.env.MONGO_DATABASE || 'lista';
+const dbPort = process.env.MONGO_PORT || 27017;
 
-const port = 4000;
-const username = 'appAdmin';
-const password = 'supersecure';
-const dbName = 'funkos';
-const url = `mongodb://${username}:${password}@localhost:27017/${dbName}`;
-const collectionName = 'listas'
+const mongoURI = `mongodb://${dbUser}:${dbPassword}@mongo-db:${dbPort}/${dbName}`;
+
+const collectionName = 'tareas'
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,10 +30,17 @@ app.post('/api/listas', async (req, res) => {
     res.redirect('/');
 });
 
+app.delete('/api/listas/:id', async (req, res) => {
+    await deleteOne(req.params.id);
+    res.redirect('/');
+});
 
+app.put('/api/listas/:id', async (req, res) => {
+    await updateDone(req.params.id, req.body.isDone);
+});
 
 async function findAll() {
-    const client = new MongoClient(url);
+    const client = new MongoClient(mongoURI);
 
     try {
         await client.connect();
@@ -46,8 +55,8 @@ async function findAll() {
     }
 }
 
-async function save(nombre) {
-    const client = new MongoClient(url, { useUnifiedTopology: true });
+async function save(nombre, isDone = false) {
+    const client = new MongoClient(mongoURI);
 
     try {
         await client.connect();
@@ -55,7 +64,8 @@ async function save(nombre) {
         const database = client.db(dbName);
         const collection = database.collection(collectionName);
 
-        const resultado = await collection.insertOne({ nombre});
+
+        const resultado = await collection.insertOne({ nombre, isDone});
 
         console.log('Resultado de la consulta:', resultado);
     } finally {
@@ -63,9 +73,43 @@ async function save(nombre) {
     }
 }
 
+async function deleteOne(id) {
+    const client = new MongoClient(mongoURI);
 
+    try {
+        await client.connect();
 
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
 
+        const idObj = new ObjectId(id);
+
+        const resultado = await collection.deleteOne({ _id: idObj });
+
+        console.log('Resultado de la consulta:', resultado);
+    } finally {
+        await client.close();
+    }
+}
+
+async function updateDone(id, isDone) {
+    const client = new MongoClient(mongoURI);
+
+    try {
+        await client.connect();
+
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const idObj = new ObjectId(id);
+
+        const resultado = await collection.updateOne({ _id: idObj }, { $set: { isDone } });
+
+        console.log('Resultado de la consulta:', resultado);
+    } finally {
+        await client.close();
+    }
+}
 
 
 
